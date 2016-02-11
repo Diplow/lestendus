a2826fbacd4be320968344bc20444909a02c5aaf
Romain Gauthier <romain.gauthier@artefact.is>
2016-02-11 21:25:30 +0100

    drone changes

--- drone.py
+++ drone.py
@@ -1,32 +1,36 @@
 import math
-from reader import BUSY_DAY_INPUT as data
 
 class Drone(object):
 
-    def __init__(self, warehouses, **kwargs):
+    def __init__(self, idx, warehouses, **kwargs):
         self.load = kwargs.get('load', -1)
+        self.row = warehouses[0].row
+        self.column = warehouses[0].column
         self.plan = None
-        self.warehouses = warehouses
+        self.idx = idx
 
     def available(self, turn):
         if not plan:
             return True
         return turn >= self.plan[-1]['date']
 
-    def create_plan(self, delivery):
-        """ delivery = (item, customer_position) """
-        warehouse = self.find_closer_route(*delivery)
+    def create_plan(self, order):
+        warehouse, number  = self.find_closer_warehouse(order.items[0], customer)
+        self.plan.append({"type":"L", "drone_id": self.idx, "warehouse_id": warehouse,\
+                "product_id": order.items[0], "number": 1})
+        self.plan.append({"type":"D", "drone_id": self.idx, "customer_id": order.id,\
+                "product_id": order.items[0], "number": 1})
 
-    def find_closer_route(self, item, client_position):
+    def find_closer_warehouse(self, order, warehouses):
         warehouse_options = list()
-        for w_idx, warehouse_inventory in enumerate(data['warehouses']):
-            if warehouse_inventory["stock"][item] > 0:
-                warehouse_options.append((w_idx, self.compute_path(data['warehouses'][, client_position)))
+        for w_idx, warehouse in enumerate(warehouses):
+            if warehouse.stock[item] > 0:
+                warehouse_options.append((w_idx, self.compute_path(warehouse, order)))
         return sorted(warehouse_options.sort(key = lambda x: x[1]))[0]
 
-        def compute_path(self, warehouse, client_position):
-            return math.sqrt( (self.position[0]-warehouse_position[0])**2 + (self.position[1]-warehouse_position[1])**2) \
-                  +  math.sqrt( (client_position[0]-warehouse_position[0])**2 + (client_position[1]-warehouse_position[1])**2)
+        def compute_path(self, warehouse, order):
+            return math.sqrt( (self.row-warehouse.row)**2 + (self.column-warehouse.column)**2) \
+                  +  math.sqrt( (order.row-warehouse.row)**2 + (order.column-warehouse.column)**2)
 
 
 
